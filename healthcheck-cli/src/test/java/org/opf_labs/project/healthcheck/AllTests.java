dc974a5220a331d6d725310a01e3bb7d1c5b453f
Carl Wilson <carl@openplanetsfoundation.org>
2013-07-10 20:59:32 +0100

    Added test suite and ProjectMetadata YAML test.

---
+++ healthcheck-cli/src/test/java/org/opf_labs/project/healthcheck/AllTests.java
@@ -0,0 +1,52 @@
+package org.opf_labs.project.healthcheck;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Class to hold general test methods and run all unit tests.
+ * 
+ * @author <a href="mailto:carl@openplanetsfoundation.org">Carl Wilson</a>.</p>
+ *         <a href="https://github.com/carlwilson">carlwilson AT github</a>.</p>
+ * @version 0.1
+ * 
+ *          Created 10 Jul 2013:15:07:22
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ ProjectMetadataTest.class })
+public class AllTests {
+	/** Root test */
+	private final static String TEST_ROOT = "org/opf_labs/project/healthcheck";
+	/** Root yaml */
+	public final static String YAML_ROOT = TEST_ROOT + "/yaml";
+	/** Test YAML */
+	public final static String TEST_YAML = YAML_ROOT + "/test.yml";
+
+	/**
+	 * @return the test OPF Metadata YAML file
+	 * @throws URISyntaxException
+	 *             if the resource can't be opened
+	 */
+	public final static File getTestYamlFile() throws URISyntaxException {
+		return getResourceAsFile(TEST_YAML);
+	}
+
+	/**
+	 * Given a string resource path and name returns a File object. Used to load
+	 * test data, not meant to be a general utility method.
+	 * 
+	 * @param resName
+	 *            the name of the resource to retrieve a file for
+	 * @return the java.io.File for the named resource
+	 * @throws URISyntaxException
+	 *             if the named resource can't be converted to a URI
+	 */
+	public final static File getResourceAsFile(String resName)
+			throws URISyntaxException {
+		return new File(ClassLoader.getSystemResource(resName).toURI());
+	}
+}
--- healthcheck-cli/src/test/java/org/opf_labs/project/healthcheck/ProjectMetadataTest.java
+++ healthcheck-cli/src/test/java/org/opf_labs/project/healthcheck/ProjectMetadataTest.java
@@ -1,60 +1,87 @@
 package org.opf_labs.project.healthcheck;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 
 import org.junit.Test;
 
 /**
  * Unit tests for ProjectMetadata class.
  * 
- * @author  <a href="mailto:carl@openplanetsfoundation.org">Carl Wilson</a>.</p>
- *          <a href="https://github.com/carlwilson">carlwilson AT github</a>.</p>
+ * @author <a href="mailto:carl@openplanetsfoundation.org">Carl Wilson</a>.</p>
+ *         <a href="https://github.com/carlwilson">carlwilson AT github</a>.</p>
  * @version 0.1
  * 
- * Created 10 Jul 2013:14:43:40
+ *          Created 10 Jul 2013:14:43:40
  */
 public class ProjectMetadataTest {
 
 	/**
-	 * Null name test for {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
+	 * Null name test for
+	 * {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
 	 */
-	@Test(expected=NullPointerException.class)
+	@Test(expected = NullPointerException.class)
 	public void testGetInstanceNullName() {
 		ProjectMetadata.getInstance(null, "vendor");
 	}
 
 	/**
-	 * Null vendor test for {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
+	 * Null vendor test for
+	 * {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
 	 */
-	@Test(expected=NullPointerException.class)
+	@Test(expected = NullPointerException.class)
 	public void testGetInstanceNullVendor() {
 		ProjectMetadata.getInstance("name", null);
 	}
 
 	/**
-	 * Empty name test for {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
+	 * Empty name test for
+	 * {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
 	 */
-	@Test(expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetInstanceEmptyName() {
 		ProjectMetadata.getInstance("", "vendor");
 	}
 
 	/**
-	 * Empty vendor test for {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
+	 * Empty vendor test for
+	 * {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(String, String)}
 	 */
-	@Test(expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetInstanceEmptyVendor() {
 		ProjectMetadata.getInstance("name", "");
 	}
 
 	/**
-	 * Null name test for {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(InputStream)}
+	 * Null name test for
+	 * {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(InputStream)}
 	 */
-	@Test(expected=NullPointerException.class)
+	@Test(expected = NullPointerException.class)
 	public void testGetInstanceInputStreamNull() {
 		ProjectMetadata.getInstance(null);
 	}
 
+	/**
+	 * Test for
+	 * {@link org.opf_labs.project.healthcheck.ProjectMetadata#getInstance(InputStream)}
+	 * 
+	 * @throws URISyntaxException
+	 *             when the test resource can't be loaded.
+	 * @throws IOException 
+	 */
+	@Test
+	public void testGetInstanceInputStream() throws URISyntaxException,
+			IOException {
+		File testYaml = AllTests.getTestYamlFile();
+		InputStream fis = new FileInputStream(testYaml);
+		ProjectMetadata pmd = ProjectMetadata.getInstance(fis);
+		assertEquals("OPF Project Healthcheck", pmd.name);
+		assertEquals("Open Planets Foundation", pmd.vendor);
+		fis.close();
+	}
 }
