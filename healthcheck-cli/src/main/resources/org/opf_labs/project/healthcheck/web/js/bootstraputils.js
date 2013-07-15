133d565b08d263c65be70c32bb30a72c41755ef4
Carl Wilson <carl@openplanetsfoundation.org>
2013-07-15 19:42:09 +0100

    Tidied and JSLint check JavaScript files.

--- healthcheck-cli/src/main/resources/org/opf_labs/project/healthcheck/web/js/bootstraputils.js
+++ healthcheck-cli/src/main/resources/org/opf_labs/project/healthcheck/web/js/bootstraputils.js
@@ -1,24 +1,27 @@
+/**
+ * @namespace bootstrapUtils
+ */
 var bootstrapUtils = {
-  pebble: function(icon, text, state, href) {
-	if (state) {
-		state= ' label-' + state;
-	} else {
-		state = "";
-	}
-	var anchor = $('<a class="label' + state + '"> ' + text + '</a>');
-	if (href) {
-		anchor.attr("href", href);
-	}
-	anchor.prepend(bootstrapUtils.getIcon(icon));
-    return anchor;
-  },
-  getIcon: function(icon) {
-	icon = "icon-" + icon
-	return $('<i>').attr({
-		class: icon
-	});
-  },
-  listPebble: function(icon, text, state, href) {
-    return $('<li>').append(bootstrapUtils.pebble(icon, text, state, href));
-  }
+    pebble: function (icon, text, state, href) {
+	    if (state) {
+		    state = ' label-' + state;
+	    } else {
+		    state = "";
+	    }
+	    var anchor = $('<a class="label' + state + '"> ' + text + '</a>');
+	    if (href) {
+		    anchor.attr("href", href);
+	    }
+	    anchor.prepend(bootstrapUtils.getIcon(icon));
+        return anchor;
+    },
+    getIcon: function (icon) {
+	    icon = "icon-" + icon;
+	    return $('<i>').attr({
+		    "class": icon
+	    });
+    },
+    listPebble: function (icon, text, state, href) {
+        return $('<li>').append(bootstrapUtils.pebble(icon, text, state, href));
+    }
 };
