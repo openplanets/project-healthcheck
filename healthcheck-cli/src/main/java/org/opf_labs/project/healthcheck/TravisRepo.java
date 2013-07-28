1a9b12bff2693df34b5bc247a5cc7ef8e07cfe34
carlwilson <carl@openplanetsfoundation.org>
2013-07-28 04:08:11 +0100

    Removed unnecessary import.

--- healthcheck-cli/src/main/java/org/opf_labs/project/healthcheck/TravisRepo.java
+++ healthcheck-cli/src/main/java/org/opf_labs/project/healthcheck/TravisRepo.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
 /**
